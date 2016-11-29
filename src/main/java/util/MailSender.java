e4dec190d5d01384d2298813fff2896d02abfd85
xyk0058 <13301007@bjtu.edu.cn>
2016-11-29 15:22:50 +0800

    first commit

--- src/main/java/util/MailSender.java
+++ src/main/java/util/MailSender.java
@@ -32,55 +32,56 @@ public class MailSender implements IMailSender {
 		// TODO Auto-generated method stub
 		try {
             String smtpFromMail = "2603653389@qq.com";  //账号  
-            String pwd = "huangfei"; //密码  
+            String pwd = "huangfei"; //密码
             int port = 25; //端口  
             String host = "mail.bjtu.edu.cn"; //邮件服务器
   
             Properties props = new Properties();  
-            props.put("mail.smtp.host", host);  
-            props.put("mail.smtp.auth", "true");  
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.auth", "true");
             Session session = Session.getDefaultInstance(props);  
             session.setDebug(false);
   
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(smtpFromMail, "QQ邮件测试"));  
-            message.addRecipient(Message.RecipientType.TO,  
-                    new InternetAddress(toMail));  
-            message.setSubject(subject);  
-            message.addHeader("charset", "UTF-8");  
-              
-            /*添加正文内容*/  
-                Multipart multipart = new MimeMultipart();  
-                BodyPart contentPart = new MimeBodyPart();  
-                contentPart.setText(content);  
-  
-                contentPart.setHeader("Content-Type", "text/html; charset=GBK");  
-            multipart.addBodyPart(contentPart);  
-              
-            /*添加附件*/  
-            for (String file : files) {  
-                File usFile = new File(file);  
-                MimeBodyPart fileBody = new MimeBodyPart();  
-                DataSource source = new FileDataSource(file);  
-                fileBody.setDataHandler(new DataHandler(source));  
-                sun.misc.BASE64Encoder enc = new sun.misc.BASE64Encoder();  
-                fileBody.setFileName("=?GBK?B?"  
-                        + enc.encode(usFile.getName().getBytes()) + "?=");  
-                    multipart.addBodyPart(fileBody);  
-                }  
-  
-                message.setContent(multipart);  
-                message.setSentDate(new Date());  
-                message.saveChanges();  
-                Transport transport = session.getTransport("smtp");  
-  
-                transport.connect(host, port, smtpFromMail, pwd);  
-                transport.sendMessage(message, message.getAllRecipients());  
-                transport.close();
+            message.setFrom(new InternetAddress(smtpFromMail, "Convert"));
+            message.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(toMail));
+			message.setSubject("Convert");
+            message.addHeader("charset", "UTF-8");
 
+            /*添加正文内容*/  
+            Multipart multipart = new MimeMultipart();
+            BodyPart contentPart = new MimeBodyPart();
+            contentPart.setText("Convert");
+            
+            contentPart.setHeader("Content-Type", "text/html; charset=GBK");  
+            multipart.addBodyPart(contentPart);
+
+            File usFile = new File(filePath);
+            MimeBodyPart fileBody = new MimeBodyPart();
+            DataSource source = new FileDataSource(filePath);
+            fileBody.setDataHandler(new DataHandler(source));
+            sun.misc.BASE64Encoder enc = new sun.misc.BASE64Encoder();
+            fileBody.setFileName("=?GBK?B?"
+                    + enc.encode(usFile.getName().getBytes()) + "?=");
+            multipart.addBodyPart(fileBody);
+  
+            message.setContent(multipart);
+            message.setSentDate(new Date());
+            message.saveChanges();
+            Transport transport = session.getTransport("smtp");
+  
+            transport.connect(host, port, smtpFromMail, pwd);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+            
         } catch (Exception e) {  
             e.printStackTrace();  
         }
 	}
-
+	
+	public static void main(String[] args) {
+		
+	}
+	
 }
